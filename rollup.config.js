00e56f06b0e606f27d940e56bff40daf9c55a6d5
Hugh Kennedy <hughsk@canva.com>
2023-05-15 13:58:38 +0100

    Add comments for rollup.config.js
    May still want to consider adding rollup/plugin-commonjs for compatibility with older npm modules, it's a sensible plugin to include by default.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,21 @@
 /* eslint-env node */
+
 import nodeResolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 
+// We have two different modes for our template bundle: "development" and
+// "production". We use development mode when we want the bundle to be generated
+// quickly, often disabling slower transforms like babel and terser. Production
+// mode is for published templates, and for testing, where it's acceptable for
+// slower build speeds in exchange for a smaller and more optimized final file.
+//
+// Running `flourish publish` will automatically set `NODE_ENV=production` as an
+// environment variable when running your build script, so this is a good way to
+// switch between development/production mode while using a single configuration
+// file.
 const is_production = process.env.NODE_ENV === "production" || !!process.env.CI;
 
+/** @type {Partial<import("rollup").RollupOptions>} */
 export default {
 	input: "src/index.js",
 	output: {
@@ -13,11 +25,19 @@ export default {
 		format: "iife"
 	},
 	plugins: [
+		// The node-resolve plugin replicates Node.js's module resolution algorithm,
+		// meaning that it allows you to import packages that have been installed
+		// into `node_modules` using a package manager like npm, pnpm or yarn.
 		nodeResolve(),
+		// As a final step, terser minifies the template JavaScript. This can slow
+		// down the build considerably, and so it's only enabled in production.
 		is_production && terser(),
 	],
-	onwarn: function (warning, warn) {
+	onwarn: function onwarn(warning, warn) {
+		// Cyclic dependencies are allowed in ES6, and such imports occur in many d3
+		// components, so suppress those rollup warnings.
 		if (warning.code === "CIRCULAR_DEPENDENCY") return;
+		// Forces rollup to fail if it's unable to resolve an import.
 		if (warning.code === "UNRESOLVED_IMPORT") {
 			throw new Error(
 				"Couldn't resolve the dependency " + warning.source +
