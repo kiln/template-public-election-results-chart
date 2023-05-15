3c3676c95dfa55e3bc9432f0645003526481900f
Olivia Lawlor <oliviajanelawlor@gmail.com>
2023-05-15 16:04:06 +0100

    Add legend notes

--- src/legend.js
+++ src/legend.js
@@ -3,20 +3,28 @@ import state from "./core/state";
 import update from "./core/update";
 import { processed_data } from "./process-data";
 
-
+// This function allows us to understand if there are any empty series' for the
+// next step.
 function emptyString (str) {
 	if (str == "") return true;
 	else if (str == 0) return false;
 	else return false;
 }
 
+// This function pulls through our data for our legend and hides any series
+// with empty values in the data using the function above.
 function updateLegend() {
 	var legend_items = processed_data
 		.filter(function(d) {
 			return !(emptyString(d.actual_value) && state.hide_empty_legend_items);
 		})
 		.map(function(d) { return d.name; });
-
+	// Creates our categorical legend (since there is no numerical colour
+	// option),appending our previously processed data with our color palette.
+	// Adding user interaction function options for when users interact with
+	// the legend including lowering opacity when clicked. Find out more
+	// information here: 
+	// https://developers.flourish.studio/sdk/getting-started/using-modules
 	legend_categorical
 		.data(legend_items, colors.getColor)
 		.filtered(state.columns_to_exclude)
@@ -28,6 +36,8 @@ function updateLegend() {
 			update();
 		});
 
+		// Updated the legend container whenever a change is made. This also
+		// only needs to be used once if you have used multiple legend types.
 	legend_container.update();
 }
 
